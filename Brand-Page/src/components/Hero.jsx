49b2f8d7ddafb0f74c8d4d85a2ddf7fc123ee6d8
dilipksahu <sahud048@gmail.com>
2024-05-19 11:17:06 +0530

    Navbar complete

---
+++ Brand-Page/src/components/Hero.jsx
@@ -0,0 +1,8 @@
+const HeroSection = () => {
+    return <main>
+        <div className="hero-content"></div>
+        <div className="hero-image"></div>
+    </main>;
+}
+
+export default HeroSection;
